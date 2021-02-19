082568c51968c5a83bd6e61b288ab7a53c507be0
Kirill Litoshko <kovzik08@gmail.com>
2021-02-19 14:39:36 +0300

    feat:update solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, brackets) {
+    if (str.length == 0) {
+        return true;
+    }
+    for (let i = 0; i < str.length; i++) {
+        for (let bracket of brackets) {
+            if (bracket[0] == str[i] && bracket[1] == str[i + 1]) {
+                let innerStr = str.slice(0, i) + str.slice(i + 2);
+                return check(innerStr, brackets);
+            }
+        }
+    }
+    return false;
+};
